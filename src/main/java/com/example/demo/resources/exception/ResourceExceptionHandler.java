3d4ea0d602d180036b65c7348deeaabf8929662a
MarcoAntonioLobo <marcoantoniolobo82@gmail.com>
2025-09-27 17:50:00 +0100

    Obtendo um usuário por id

---
+++ src/main/java/com/example/demo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.example.demo.resources.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.example.demo.services.exceptions.ObjectNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+        StandardError err = new StandardError(
+            Instant.now(),
+            HttpStatus.NOT_FOUND.value(),
+            "Not Found",
+            e.getMessage(),
+            request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+}
--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.example.demo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.User;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,10 @@ public class UserService {
         return repo.findAll();
     }
 
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+            "User not found! Id: " + id
+        ));
+    }
 }
