3fa35c281d8374285d8f6cea87b4001c174204ab
MarcoAntonioLobo <marcoantoniolobo82@gmail.com>
2025-09-28 16:28:04 +0100

    Projeção dos dados do autor com DTO

--- src/main/java/com/example/demo/config/Instantiation.java
+++ src/main/java/com/example/demo/config/Instantiation.java
@@ -10,39 +10,39 @@ import org.springframework.context.annotation.Configuration;
 
 import com.example.demo.domain.Post;
 import com.example.demo.domain.User;
+import com.example.demo.dto.AuthorDTO;
 import com.example.demo.repository.PostRepository;
 import com.example.demo.repository.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
-	@Autowired
-	private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-	@Autowired
-	private PostRepository postRepository;
+    @Autowired
+    private PostRepository postRepository;
 
-	@Override
-	public void run(String... args) throws Exception {
+    @Override
+    public void run(String... args) throws Exception {
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
-		userRepository.deleteAll();
+        userRepository.deleteAll();
+        postRepository.deleteAll();
 
-		postRepository.deleteAll();
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Gray", "bob@gmail.com");
 
-		User maria = new User(null, "Maria Brown", "maria@gmail.com");
-		User alex = new User(null, "Alex Green", "alex@gmail.com");
-		User bob = new User(null, "Bob Gray", "bob@gmail.com");
+        userRepository.saveAll(List.of(maria, alex, bob));
 
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!!!", "Vou viajar pra São Paulo. Abraços",
-				maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!!!", "Acordei feliz hoje", maria);
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!!!",
+                "Vou viajar pra São Paulo. Abraços", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!!!",
+                "Acordei feliz hoje", new AuthorDTO(maria));
 
-		userRepository.saveAll(List.of(maria, alex, bob));
-
-		postRepository.saveAll(List.of(post1, post2));
-
-	}
+        postRepository.saveAll(List.of(post1, post2));
+    }
 }
